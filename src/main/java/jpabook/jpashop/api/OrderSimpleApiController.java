79728a837b21690df34a2655880ff8b59fa8c87b
hahyuning <hahyuning@gmail.com>
2021-12-28 14:59:38 +0900

    RestAPI

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -16,76 +16,88 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 /**
- * x To One 조회 성능 최적화
+ *
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
  * Order
  * Order -> Member
  * Order -> Delivery
+ *
  */
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
-    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
-    // 엔티티 직접 노출
+    // v1: 엔티티 직접 노출
+    // - Hibernate5Module 등록, LAZY=null 처리
+    // - 양방향 관계 문제 발생 -> @JsonIgnore
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> orderV1() {
+    public List<Order> ordersV1() {
+
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
-            order.getMember().getName(); // Lazy 강제 초기화
-            order.getDelivery().getAddress(); // Lazy 초기화
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환
         }
         return all;
     }
 
-    // 엔티티를 dto로 변환
+    // v2: 엔티티를 조회해서 dto로 변환 (fetch join 사용 x)
+    // 단점: 지연로딩으로 쿼리 N번 호출
     @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> orderV2() {
+    public List<SimpleOrderDto> ordersV2() {
+
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-
-        // 1 + N 문제 발생
-        List<SimpleOrderDto> result = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
-                .collect(Collectors.toList());
-
-        return result;
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
     }
-
-    // 패치 조인 최적화 -> 많이 씀
-    @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> orderV3() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-
-        List<SimpleOrderDto> result = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
-                .collect(Collectors.toList());
-
-        return result;
-    }
-
-    // jpa에서 dto 직접 조회
-    @GetMapping("/api/v4/simple-orders")
-    public List<OrderSimpleQueryDTo> orderV4() {
-        return orderSimpleQueryRepository.findOrderDtos();
-    }
-
 
     @Data
     static class SimpleOrderDto {
         private Long orderId;
         private String name;
-        private LocalDateTime orderData;
+        private LocalDateTime orderDate; //주문시간
         private OrderStatus orderStatus;
         private Address address;
 
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
             name = order.getMember().getName(); // Lazy 초기화
-            orderData = order.getOrderDate();
+            orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
             address = order.getDelivery().getAddress(); // Lazy 초기화
         }
     }
+
+    // v3: 엔티티를 조회해서 dto로 변환 (fetch join 사용 o)
+    // fetch join으로 쿼리 1번 호출
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+    }
+
+    // v4: jpa에서 dto로 바로 조회
+    // - 쿼리 1번 호출
+    // - select 절에서 원하는 데이터만 선택해서 조회
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDTo> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+    // 정리: 쿼리 방식 선택 권장 순서
+    // 1. 우선 엔티티를 dto로 변환하는 방법을 선택한다.
+    // 2. 필요하면 페치 조인으로 성능을 최적화 한다. -> 대부분의 성능 이슈가 해결된다.
+    // 3. 그래도 안되면 dto로 직접 조회하는 방법을 사용한다.
+    // 4. 최후의 방법은 jpa가 제공하는 네이티브 sql이나 스프링 jdbc template를 사용해서 sql을 직접 사용한다.
 }
